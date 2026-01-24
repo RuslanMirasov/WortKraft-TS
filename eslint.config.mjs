a8706b6f43777fab802563891dcf456c5ffda5a7
Ruslan <info@mirasov.dev>
2026-01-24 23:00:52 +0100

    change next version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,20 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import pluginNext from "@next/eslint-plugin-next";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import pluginNext from '@next/eslint-plugin-next';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     languageOptions: {
       parser: tseslint.parser,
       parserOptions: {
-        sourceType: "module",
-        ecmaVersion: "latest",
+        sourceType: 'module',
+        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        project: "./tsconfig.json",
+        project: './tsconfig.json',
       },
       globals: {
         ...globals.browser,
@@ -24,12 +24,17 @@ export default defineConfig([
     plugins: {
       js,
       react: pluginReact,
-      "@next/next": pluginNext,
+      '@next/next': pluginNext,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...tseslint.configs.recommended.rules,
       ...pluginReact.configs.recommended.rules,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
 ]);
