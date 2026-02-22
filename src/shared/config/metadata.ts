b27161767ecebd33b2d3cdde34ef6e5b0e38fbd9
Ruslan <info@mirasov.dev>
2026-02-22 22:40:19 +0100

    change icons

--- src/shared/config/metadata.ts
+++ src/shared/config/metadata.ts
@@ -74,10 +74,10 @@ export async function buildLocaleMetadata(locale: string): Promise<Metadata> {
       icon: [
         { url: '/favicon.ico', sizes: 'any' },
         { url: '/favicon.svg', sizes: 'any', type: 'image/svg+xml' },
-        { url: `/logo/favicon-16x16.png`, sizes: '16x16', type: 'image/png' },
-        { url: `/logo/favicon-32x32.png`, sizes: '32x32', type: 'image/png' },
+        { url: `/favicon-16x16.png`, sizes: '16x16', type: 'image/png' },
+        { url: `/favicon-32x32.png`, sizes: '32x32', type: 'image/png' },
       ],
-      apple: [{ url: `/logo/apple-touch-icon.png`, sizes: '180x180', type: 'image/png' }],
+      apple: [{ url: `/apple-touch-icon.png`, sizes: '180x180', type: 'image/png' }],
     },
 
     robots: {
